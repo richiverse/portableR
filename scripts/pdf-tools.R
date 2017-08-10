df7a372807e3af08c2b377552ffb3e5c0ce33134
richiverse <richard.fernandez.nyc@gmail.com>
2017-08-10 14:23:30 -0400

    convert to csv, not txt

--- scripts/pdf-tools.R
+++ scripts/pdf-tools.R
@@ -1,4 +1,4 @@
 library(pdftools)
 ifile <- commandArgs(trailingOnly=TRUE)[1]
 txt <- pdf_text(ifile)
-write.csv(txt, file=paste(ifile, '_converted.txt', sep=""))
+write.csv(txt, file=paste(ifile, '_converted.csv', sep=""))
